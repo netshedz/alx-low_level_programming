65c5b92952a2a365b8af7c3770cb03a666beb16e
netshedz <netshedzodzhivhuho06@gmail.com>
2022-08-11 11:24:47 -0700

    tasks

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the lists.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nelem;
+
+	nelem = 0;
+	while (h != NULL)
+	{
+		if (h-> == NULL)
+			printf( "[%d] %s\n", 0, "(nil)");
+		else
+			h = h->next;
+		nelem++;
+	}
+	return (nelem);
+
+}
