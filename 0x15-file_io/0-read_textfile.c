86dfaa9c84aecc1ad56b84ae5c937b4255c6b945
netshedz <netshedzodzhivhuho06@gmail.com>
2022-08-22 15:26:28 -0700

    tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,6 +19,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
 	nrd = read(fd, buf, letters);
 	nwr = write(STDOUT_FILENO, buf, nrd);
 
